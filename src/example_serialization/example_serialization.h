05378843b6871dd13471b71956f68128c3b108c1
板垣 衛 <ita_gaki@wd5.so-net.ne.jp>
2014-07-23 06:29:14 +0900

    ・シリアライズ追加中（一旦コミット）。 　※とりあえず動作する状態。サンプルプログラムを調整中。

---
+++ src/example_serialization/example_serialization.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#ifndef GASHA_INCLUDED_EXAMPLE_SERIALIZATION_H
+#define GASHA_INCLUDED_EXAMPLE_SERIALIZATION_H
+
+//--------------------------------------------------------------------------------
+// exmaple_serialization.h
+// シリアライズテスト
+//
+// Gakimaru's researched and standard library for C++ - GASHA
+//   Copyright (c) 2014 Itagaki Mamoru
+//   Released under the MIT license.
+//     https://github.com/gakimaru/gasha_examples/blob/master/LICENSE
+//--------------------------------------------------------------------------------
+
+//----------------------------------------
+//シリアライズテスト
+void example_serialization();
+
+#endif//GASHA_INCLUDED_EXAMPLE_SERIALIZATION_H
+
+// End of file
